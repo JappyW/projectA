319755d4e60eadcc27e104815c68b646ff35a770
Vitalii_Dr <dron.v.wezom@gmail.com>
2020-10-29 17:08:39 +0200

    f

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 import config from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
-import rootRoutes from "./server/routes/RootRoutes";
-import multer from "multer";
+fimport rootRoutes from "./server/routes/RootRoutes";
 import { SERVER_PORT_DEFAULT } from "./server/constants/server.env.json";
 import { CLIENT_DOMAIN_DEFAULT } from "./server/constants/server.env.json";
 import STATUS from "./server/constants/status.code.env.json";
-import cron from "node-cron";
 
 config.config();
 
@@ -18,12 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: CLIENT_DOMAIN_DEFAULT }));
 app.use(rootRoutes);
-app.use((err, req, res, next) => {
-  if (err instanceof multer.MulterError) {
-    // A Multer error occurred when uploading.
-    res.status(STATUS.INCORRECT).send(err.message);
-  }
-});
 
 // when a random route is inputed
 app.get("*", (req, res) =>
