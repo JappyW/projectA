0a98b936d57ca739db026bc9ea65c8023eabd4bb
Vitalii_Dr <dron.v.wezom@gmail.com>
2020-10-29 15:00:56 +0200

    project a front

--- backend/api/server/constants/regexpConstanst.js
+++
@@ -1,4 +0,0 @@
-export const regexps = {
-  IMAGE_REGEXP: /\/(jpg|jpeg|png|gif|webp)$/,
-  END_OF_MIMETYPE: /\/\w+$/
-};
--- backend/api/server/routes/ContactsRoutes.js
+++
@@ -1,8 +0,0 @@
-import Router from 'express';
-import ContactsController from '../controllers/ContactsController';
-const router = Router();
-
-router.get('/', ContactsController.getAllContacts);
-router.post('/create', ContactsController.createContact);
-
-export default router;
