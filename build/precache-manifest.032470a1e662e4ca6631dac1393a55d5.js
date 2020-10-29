a46be9251568d7647183d5138cf8eca4df720d55
Vitalii_Dr <dron.v.wezom@gmail.com>
2020-10-29 16:35:28 +0200

    f

---
+++ build/precache-manifest.032470a1e662e4ca6631dac1393a55d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b986e9a2aadd36953c001eef7f6e4ddb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db9f551274e89160b1e2",
+    "url": "/static/css/main.907dfb17.chunk.css"
+  },
+  {
+    "revision": "1984beaec783a1cd6ef6",
+    "url": "/static/js/2.34a6d07a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.34a6d07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9f551274e89160b1e2",
+    "url": "/static/js/main.d9a778d6.chunk.js"
+  },
+  {
+    "revision": "7123c836dd9e39bc8248",
+    "url": "/static/js/runtime-main.051cbb2f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/react-logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
